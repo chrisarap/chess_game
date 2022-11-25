2866f59a300e6bd6d6f1c605705983a9f19e3e69
chrisarap <carteaga460@gmail.com>
2022-11-24 22:58:40 -0400

    board created

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
-server.listen(3001, () => console.log('server listening on port 3001');
+app.use('/public', express.static(__dirname + '/public'));
+app.set('view engine', 'pug');
+
+
+app.get('/', (req, res) => res.render(__dirname + '/views/index.pug'));
+
+app.listen(3001, () => console.log('server listening on port 3001'));
