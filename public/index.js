e22d352f36ad05d64857b8acb799a3bf0ed022c4
chrisarap <carteaga460@gmail.com>
2022-12-05 18:18:39 -0400

    color of pieces added to the piecesCode object

--- public/index.js
+++ public/index.js
@@ -1,65 +1,65 @@
 
 
 let piecesCode = {
-	br: '265c',
-	wr: '2656',
-	bkg: '265e',
-	wkg: '2658',
-	bb: '265d',
-	wb: '2657',
-	bp: '265f',
-	wp: '2659',
-	bk: '265a',
-	wk: '2654',
-	bq: '265b',
-	wq: '2655'
+	blackRook: { code: '265c', colorPiece: 'black' },
+	whiteRook: { code: '2656', colorPiece: 'white' },
+	blackKnight: { code: '265e', colorPiece: 'black' },
+	whiteKnight: { code: '2658', colorPiece: 'white' },
+	blackBishop: { code: '265d', colorPiece: 'black' },
+	whiteBishop: { code: '2657', colorPiece: 'white' },
+	blackPawn: { code: '265f', colorPiece: 'black' },
+	whitePawn: { code: '2659', colorPiece: 'white' },
+	blackKing: { code: '265a', colorPiece: 'black' },
+	whiteKing: { code: '2654', colorPiece: 'white' },
+	blackQueen: { code: '265b', colorPiece: 'black' },
+	whiteQueen: { code: '2655', colorPiece: 'white' }
 }
 
 let initialPos = {
 	// rook
-	'a1': piecesCode.wr,
-	'h1': piecesCode.wr,
-	'a8': piecesCode.br,
-	'h8': piecesCode.br,
+	'a1': piecesCode.whiteRook,
+	'h1': piecesCode.whiteRook,
+	'a8': piecesCode.blackRook,
+	'h8': piecesCode.blackRook,
 
 	//knight
-	'b1': piecesCode.wkg,
-	'g1': piecesCode.wkg,
-	'b8': piecesCode.bkg,
-	'g8': piecesCode.bkg,
+	'b1': piecesCode.whiteKnight,
+	'g1': piecesCode.whiteKnight,
+	'b8': piecesCode.blackKnight,
+	'g8': piecesCode.blackKnight,
 
 	// bishop
-	'c1': piecesCode.wb,
-	'f1': piecesCode.wb,
-	'c8': piecesCode.bb,
-	'f8': piecesCode.bb,
+	'c1': piecesCode.whiteBishop,
+	'f1': piecesCode.whiteBishop,
+	'c8': piecesCode.blackBishop,
+	'f8': piecesCode.blackBishop,
 
 	// king
-	'e1': piecesCode.wk,
-	'e8': piecesCode.bk,
+	'e1': piecesCode.whiteKing,
+	'e8': piecesCode.blackKing,
 
 	// queen
-	'd1': piecesCode.wq,
-	'd8': piecesCode.bq,
+	'd1': piecesCode.whiteQueen,
+	'd8': piecesCode.blackQueen,
 
 	//pawn
-	'a2': piecesCode.wp,
-	'b2': piecesCode.wp,
-	'c2': piecesCode.wp,
-	'd2': piecesCode.wp,
-	'e2': piecesCode.wp,
-	'f2': piecesCode.wp,
-	'g2': piecesCode.wp,
-	'h2': piecesCode.wp,
+	'a2': piecesCode.whitePawn,
+	'b2': piecesCode.whitePawn,
+	'c2': piecesCode.whitePawn,
+	'd2': piecesCode.whitePawn,
+	'e2': piecesCode.whitePawn,
+	'f2': piecesCode.whitePawn,
+	'g2': piecesCode.whitePawn,
+	'h2': piecesCode.whitePawn,
 
-	'a7': piecesCode.bp,
-	'b7': piecesCode.bp,
-	'c7': piecesCode.bp,
-	'd7': piecesCode.bp,
-	'e7': piecesCode.bp,
-	'f7': piecesCode.bp,
-	'g7': piecesCode.bp,
-	'h7': piecesCode.bp,
+	'a7': piecesCode.blackPawn,
+	'b7': piecesCode.blackPawn,
+	'c7': piecesCode.blackPawn,
+	'd7': piecesCode.blackPawn,
+	'e7': piecesCode.blackPawn,
+	'f7': piecesCode.blackPawn,
+	'g7': piecesCode.blackPawn,
+	'h7': piecesCode.blackPawn
 };
 
 let board = {};
@@ -75,8 +75,10 @@ let createBoardObj = () => {
 			// first return 'a1'
 			let coordinate = String.fromCharCode(letterFromAscii) + (i + 1);
 			let color = boardNumber % 2 ? squareColor[0] : squareColor[1];
+			let pieceData = initialPos[coordinate];
+			console.log('hey ', pieceData, ' ', coordinate)
 
-			let pieceCode = typeof initialPos[coordinate] == 'string' ? initialPos[coordinate] : null;
+			let pieceCode = !pieceData ? null : pieceData.code;
 			let squareData = {
 				[coordinate]: {
 					number: boardNumber,
