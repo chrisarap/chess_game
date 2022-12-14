73527e4fd71b29852c3a5b8b546f5830faba4b7f
chrisarap <carteaga460@gmail.com>
2022-12-14 18:58:13 -0400

    code property deleted from piecedata object

--- public/index.js
+++ public/index.js
@@ -1,16 +1,16 @@
 let piecesData = {
-	blackRook: { code: '265c', colorPiece: 'black', srcImg: './public/black_rook.png', initialPosition: ['a8', 'h8'] },
-	whiteRook: { code: '2656', colorPiece: 'white', srcImg: './public/white_rook.png', initialPosition: ['a1', 'h1'] },
-	blackKnight: { code: '265e', colorPiece: 'black', srcImg: './public/black_knight.png', initialPosition: ['b8', 'g8'] },
-	whiteKnight: { code: '2658', colorPiece: 'white', srcImg: './public/white_knight.png', initialPosition: ['b1', 'g1'] },
-	blackBishop: { code: '265d', colorPiece: 'black', srcImg: './public/black_bishop.png', initialPosition: ['c8', 'f8'] },
-	whiteBishop: { code: '2657', colorPiece: 'white', srcImg: './public/white_bishop.png', initialPosition: ['c1', 'f1'] },
-	blackPawn: { code: '265f', colorPiece: 'black', srcImg: './public/black_pawn.png', srcImg: './public/black_pawn.png', initialPosition: ['a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7'] },
-	whitePawn: { code: '2659', colorPiece: 'white', srcImg: './public/white_pawn.png', initialPosition: ['a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2'] },
-	blackKing: { code: '265a', colorPiece: 'black', srcImg: './public/black_king.png', initialPosition: ['e8'] },
-	whiteKing: { code: '2654', colorPiece: 'white', srcImg: './public/white_king.png', initialPosition: ['e1'] },
-	blackQueen: { code: '265b', colorPiece: 'black', srcImg: './public/black_queen.png', initialPosition: ['d8'] },
-	whiteQueen: { code: '2655', colorPiece: 'white', srcImg: './public/white_queen.png', initialPosition: ['d1'] }
+	blackRook: { colorPiece: 'black', srcImg: './public/black_rook.png', initialPosition: ['a8', 'h8'] },
+	whiteRook: { colorPiece: 'white', srcImg: './public/white_rook.png', initialPosition: ['a1', 'h1'] },
+	blackKnight: { colorPiece: 'black', srcImg: './public/black_knight.png', initialPosition: ['b8', 'g8'] },
+	whiteKnight: { colorPiece: 'white', srcImg: './public/white_knight.png', initialPosition: ['b1', 'g1'] },
+	blackBishop: { colorPiece: 'black', srcImg: './public/black_bishop.png', initialPosition: ['c8', 'f8'] },
+	whiteBishop: { colorPiece: 'white', srcImg: './public/white_bishop.png', initialPosition: ['c1', 'f1'] },
+	blackPawn: { colorPiece: 'black', srcImg: './public/black_pawn.png', initialPosition: ['a7', 'b7', 'c7', 'd7', 'e7', 'f7', 'g7', 'h7'] },
+	whitePawn: { colorPiece: 'white', srcImg: './public/white_pawn.png', initialPosition: ['a2', 'b2', 'c2', 'd2', 'e2', 'f2', 'g2', 'h2'] },
+	blackKing: { colorPiece: 'black', srcImg: './public/black_king.png', initialPosition: ['e8'] },
+	whiteKing: { colorPiece: 'white', srcImg: './public/white_king.png', initialPosition: ['e1'] },
+	blackQueen: { colorPiece: 'black', srcImg: './public/black_queen.png', initialPosition: ['d8'] },
+	whiteQueen: { colorPiece: 'white', srcImg: './public/white_queen.png', initialPosition: ['d1'] }
 }
 
 let initialPos = {
@@ -129,7 +129,6 @@ let createHtmlBoard = () => {
 	document.body.appendChild(boardHtml);
 };
 
-let arr = [];
 
 let createHtmlSquares = () => {
 
@@ -155,8 +154,10 @@ let createHtmlSquares = () => {
 		//img.setAttribute('src', coordinateObject.srcImg)
 
 		square.addEventListener('click', e => {
-			console.log(e.path[1].id);
-			//arr.push(e.target.id);
+			let coordinateSelected = e.path[1].id;
+			movementArr.push(coordinateSelected);
+			movePiece();
+			//movementArr.push(e.target.id);
 			//movePiece();
 		});
 
@@ -177,17 +178,18 @@ let createHtmlSquares = () => {
 	}
 };
 
+/*
 let renderPiece = (htmlElement, coordinateObject, coordinateName) => {
 	htmlElement.textContent = typeof coordinateObject.piece == 'string'
 		? coordinateName + createIcon(coordinateObject.piece)
 		: coordinateName;
 };
-
+*/
 let initialPieces = () => {
 	console.log(board);
 	for (const piece in piecesData) {
 		let pieceData = piecesData[piece];
-		let position = pieceData.initialPosition; // arr
+		let position = pieceData.initialPosition; // movementArr
 		for (const i in position) {
 			console.log(position[i], piece)
 			board[position[i]].piece = piece;
@@ -213,54 +215,67 @@ let renderPieces = () => {
 };
 
 
+let movementArr = [];
 let movePiece = () => {
 
-	let first = board[arr[0]];
 
-	let aux = '';
+
+	let first = board[movementArr[0]];
+	console.log('piece selected: ', first);
 
 	let firstElement = document.getElementById(first.name);
-	let firstElementColor = first.color;
+	let aux = '';
 
-	if (arr.length == 1) {
+	if (movementArr.length == 1) {
 
-		if (first.piece == null) {
-			arr = [];
-			console.log('you cannot select empty space');
-		} else {
-			firstElement.style.background = 'grey';
-			console.log('first: ', first);
-		}
+	} else if (movementArr.length == 2) {
 
-
-	} else if (arr.length == 2) {
-
-		let second = board[arr[1]];
-
-		if (first != second && first.colorPiece != second.colorPiece) {
-			let secondElement = document.getElementById(second.name);
-
-			console.log('first: ', first, '\nsecond: ', second);
-			aux = first.piece;
-			let auxColorPiece = first.colorPiece;
-
-			second.piece = first.piece;
-			second.colorPiece = auxColorPiece;
-
-			first.piece = null;
-			first.colorPiece = null;
-			aux = '';
-			arr = [];
-
-			renderPiece(firstElement, first, first.name);
-			renderPiece(secondElement, second, second.name);
-
-			console.log(board);
-		}
-
-		firstElement.style.background = firstElementColor;
-		arr = [];
 	}
+
+
+	/*
+	
+		let firstElementColor = first.color;
+	
+		if (movementArr.length == 1) {
+	
+			if (first.piece == null) {
+				movementArr = [];
+				console.log('you cannot select empty space');
+			} else {
+				firstElement.style.background = 'grey';
+				console.log('first: ', first);
+			}
+	
+	
+		} else if (movementArr.length == 2) {
+	
+			let second = board[movementArr[1]];
+	
+			if (first != second && first.colorPiece != second.colorPiece) {
+				let secondElement = document.getElementById(second.name);
+	
+				console.log('first: ', first, '\nsecond: ', second);
+				aux = first.piece;
+				let auxColorPiece = first.colorPiece;
+	
+				second.piece = first.piece;
+				second.colorPiece = auxColorPiece;
+	
+				first.piece = null;
+				first.colorPiece = null;
+				aux = '';
+				movementArr = [];
+	
+				renderPiece(firstElement, first, first.name);
+				renderPiece(secondElement, second, second.name);
+	
+				console.log(board);
+			}
+	
+			firstElement.style.background = firstElementColor;
+			movementArr = [];
+		}*/
 };
 
 createBoardObj();
